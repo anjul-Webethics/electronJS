d027710978256ad2562d8430b136a3d9af76957e
anjul-Webethics <anjul.webethics@gmail.com>
2021-12-07 12:27:16 +0530

    some changes

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 2000,
+    width: 1,
+    height: 2,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
